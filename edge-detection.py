a28b46afbb6148a188bf3a2c5bd08ef2678d8d6d
nabagata <ngsaha234@gmail.com>
2020-05-01 12:17:08 +0530

    cv codes

---
+++ edge-detection.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('crossword_puzzle.jpg',0)
+
+laplacian = cv2.Laplacian(img,cv2.CV_64F)
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+
+plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
+
---
+++ plate-detection.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+import  imutils
+import pytesseract 
+
+# Read the image file
+image = cv2.imread('demo.jpg')
+
+# Resize the image - change width to 500
+image = imutils.resize(image, width=500)
+
+# Display the original image
+cv2.imshow("Original Image", image)
+
+# RGB to Gray scale conversion
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("1 - Grayscale Conversion", gray)
+
+# Noise removal with iterative bilateral filter(removes noise while preserving edges)
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
+cv2.imshow("2 - Bilateral Filter", gray)
+
+# Find Edges of the grayscale image
+edged = cv2.Canny(gray, 170, 200)
+cv2.imshow("4 - Canny Edges", edged)
+
+# Find contours based on Edges
+(new, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts=sorted(cnts, key = cv2.contourArea, reverse = True)[:30] #sort contours based on their area keeping minimum required area as '30' (anything smaller than this will not be considered)
+NumberPlateCnt = None #we currently have no Number plate contour
+
+# loop over our contours to find the best possible approximate contour of number plate
+count = 0
+for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:  # Select the contour with 4 corners
+            NumberPlateCnt = approx #This is our approx Number Plate Contour
+            break
+
+
+# Drawing the selected contour on the original image
+cv2.drawContours(image, [NumberPlateCnt], -1, (0,255,0), 3)
+cv2.imshow("Final Image With Number Plate Detected", image)
+
+# Looping through the identified contours 
+# Then rectangular part is cropped and passed on 
+# to pytesseract for extracting text from it 
+# Extracted text is then written into the text file 
+#for cnt in contours: 
+x, y, w, h = cv2.boundingRect(NumberPlateCnt) 
+  
+# Drawing a rectangle on copied image 
+rect = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+  
+# Cropping the text block for giving input to OCR 
+cropped = image[y:y + h, x:x + w] 
+  
+# Apply OCR on the cropped image 
+text = pytesseract.image_to_string(cropped) 
+
+print(text)  
+
+cv2.waitKey(0) 
